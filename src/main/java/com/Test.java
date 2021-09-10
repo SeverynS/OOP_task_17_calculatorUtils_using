c501eb37ca9876ef0f0bc44244084f5a2de64460
SeverynZ <s.zlochovsky@gmail.com>
2021-09-10 16:15:34 +0300

    Initial commit

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,9 @@
+package com;
+
+import com.utils.CalculatorUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(CalculatorUtils.substraction(100, 37));
+    }
+}
